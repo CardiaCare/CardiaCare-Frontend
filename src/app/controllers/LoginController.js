8d9ca8e7f029965fe745294fa3a21328971e7a40
nikolay <nicklebedev14@gmail.com>
2016-12-19 14:20:47 +0300

    auth service introduces

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,52 +1,32 @@
-(function(){
+(function () {
 
-  angular
-    .module('app')
-    .controller('LoginController', ['$scope', '$http', '$base64',
-      LoginController
-    ]);
+    angular
+        .module('app')
+        .controller('LoginController', ['$scope', '$rootScope', 'AuthService', 'AUTH_EVENTS',
+            LoginController
+        ]);
 
-  function LoginController($scope, $http, $base64) {
-    var vm = this;
+    function LoginController($scope, $rootScope, AuthService, AUTH_EVENTS) {
+        var vm = this;
 
-    vm.credentials = {};
-    
-    vm.login = function() {
-        var email = vm.credentials.email;
-        var password = vm.credentials.password;
-        
-        if(email === '' || password === ''){
-            alert("Empty credentials");
-            return;
-        }
-        var authData = {email: email, password: password};
+        vm.credentials = {};
 
-        $http({
-            method: 'POST',
-            url: 'http://api.cardiacare.ru/tokens',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            data: authData
-        }).then(function successCallback(response) {
-                $http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"};
-                /*
-                * Server side authentication is a little bit stupid
-                * So the workflow is following:
-                * We get token using /tokens request
-                * Our token is (suddenly!) is new login
-                * Basic auth requires login and password encoded with base64 / e.g. base64(login:password) /
-                * As we don't have a password we just concatenate our token and ":"
-                * FIXME: It's needed to be in separated service or smth like this
-                */
-                $http.defaults.headers.common.Authorization = 'Basic ' + $base64.encode(response.data+":");
-                
-                $scope.isAuthorized = true;
-        }, function errorCallback(response) {
-                $scope.isAuthorized = false;
-        });
-    };
-  }
+        vm.login = function () {
+            var email = vm.credentials.email;
+            var password = vm.credentials.password;
+
+            if (email === '' || password === '') {
+                alert("Empty credentials");
+                return;
+            }
+            var credentials = {email: email, password: password};
+            AuthService.login(credentials).then(function (user) {
+                $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+                // $scope.setCurrentUser(user);
+            }, function () {
+                $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+            });
+        };
+    }
 
 })();
