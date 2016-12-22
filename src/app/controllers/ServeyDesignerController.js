c6722b507987b220036305f287f523c8b1dc1f72
nikolay <nicklebedev14@gmail.com>
2016-12-22 13:49:25 +0300

    fix servey type error

--- src/app/controllers/ServeyDesignerController.js
+++ src/app/controllers/ServeyDesignerController.js
@@ -1,35 +1,35 @@
 (function () {
 
-angular
+    angular
         .module('app')
         .controller('ServeyDesignerController', ['$scope',
-                ServeyDesignerController
+            ServeyDesignerController
         ]);
-        function ServeyDesignerController($scope) {
-            $scope.items = [];
-            
-            $scope.qtypes = [
-                {
-                    description: "Dichotomois"
-                },
-                {
-                    description: "Open"
-                },
-                {
-                    description: "Single Selected"
-                }
-            ];
-            
-        
-            $scope.itemsToAdd = [{
-                firstName: '',
-                lastName: ''
-             }];
-            
-            $scope.fix = function(itemToAdd) {
-                var index = $scope.itemsToAdd.indexOf(itemToAdd);
-                $scope.itemsToAdd.splice(index, 1);
-                $scope.items.push(angular.copy(itemToAdd))
+    function ServeyDesignerController($scope) {
+        $scope.items = [];
+        $scope.chosenType = '';
+        $scope.qtypes = [
+            {
+                description: "Dichotomois"
+            },
+            {
+                description: "Open"
+            },
+            {
+                description: "Single Selected"
+            }
+        ];
+
+
+        $scope.itemsToAdd = [{
+            firstName: '',
+            lastName: ''
+        }];
+
+        $scope.fix = function (itemToAdd) {
+            var index = $scope.itemsToAdd.indexOf(itemToAdd);
+            $scope.itemsToAdd.splice(index, 1);
+            $scope.items.push(angular.copy(itemToAdd))
         };
 
         $scope.addNew = function () {
