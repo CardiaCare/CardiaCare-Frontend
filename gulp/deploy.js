aefdf0908411f6ee9b5884c25ce617c62d8961b7
nikolay <nicklebedev14@gmail.com>
2017-01-20 19:28:39 +0300

    gulp deploy

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -1,9 +1,72 @@
 'use strict';
 
 var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
+// gulp-util - https://www.npmjs.com/package/gulp-util
+var gutil = require('gulp-util');
 
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-      .pipe(ghPages());
+// Minimist - https://www.npmjs.com/package/minimist
+var argv = require('minimist')(process.argv);
+
+// gulp-rsync - https://www.npmjs.com/package/gulp-rsync
+var rsync = require('gulp-rsync');
+
+// gulp-prompt - https://www.npmjs.com/package/gulp-prompt
+var prompt = require('gulp-prompt');
+
+// gulp-if - https://www.npmjs.com/package/gulp-if
+var gulpif = require('gulp-if');
+gulp.task('deploy', function () {
+    // Dirs and Files to sync
+    var rsyncPaths = [gulp.paths.dist, 'lang', 'lib', 'templates', './*.php', './style.css'];
+
+    // Default options for rsync
+    var rsyncConf = {
+        progress: true,
+        incremental: true,
+        relative: true,
+        emptyDirectories: true,
+        recursive: true,
+        clean: true,
+        exclude: []
+    };
+
+    // Staging
+    if (argv.staging) {
+
+        rsyncConf.hostname = ''; // hostname
+        rsyncConf.username = ''; // ssh username
+        rsyncConf.destination = ''; // path where uploaded files go
+
+        // Production
+    } else if (argv.production) {
+
+        rsyncConf.hostname = 'cardiacare.ru'; // hostname
+        rsyncConf.username = 'api'; // ssh username
+        rsyncConf.destination = '/home/api/dashboard'; // path where uploaded files go
+
+
+        // Missing/Invalid Target
+    } else {
+        throwError('deploy', gutil.colors.red('Missing or invalid target'));
+    }
+
+
+    // Use gulp-rsync to sync the files
+    return gulp.src(rsyncPaths)
+        .pipe(gulpif(
+            argv.production,
+            prompt.confirm({
+                message: 'Heads Up! Are you SURE you want to push to PRODUCTION?',
+                default: false
+            })
+        ))
+        .pipe(rsync(rsyncConf));
+
 });
+
+function throwError(taskName, msg) {
+    throw new gutil.PluginError({
+        plugin: taskName,
+        message: msg
+    });
+}
