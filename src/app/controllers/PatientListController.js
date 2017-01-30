58ca6cc628d6a84fe42d86ba4eb4e6e4554a3671
yzavyalo <yzavyalo@cs.karelia.ru>
2017-01-30 12:44:50 +0300

    UI for patient+questionnaire

--- src/app/controllers/PatientListController.js
+++ src/app/controllers/PatientListController.js
@@ -3,11 +3,11 @@
 
     angular
         .module('app')
-        .controller('PatientListController', ['$scope', 'Restangular',
+        .controller('PatientListController', ['$scope', 'Restangular', '$translate', '$mdDialog',
             PatientListController
         ]);
 
-    function PatientListController($scope, Restangular) {
+    function PatientListController($scope, Restangular, $translate, $mdDialog) {
         $scope.patients = [];
         $scope.activated = true;
 
@@ -17,7 +17,57 @@
                     $scope.patients = response;
                     $scope.activated = false;
                 });
-                
+
+
+        $scope.dialogQuestionnaire = function (ev, patient_id) {
+            $scope.questionniares=[];
+
+            $mdDialog.show({
+                controller: SurveyAddDialogController,
+                templateUrl: 'addqst.tmpl.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose: true
+            });
+
+            function SurveyAddDialogController($scope, $mdDialog) {
+                console.log(patient_id);
+                //get all surveys
+                Restangular.all('survey')
+                        .getList()
+                        .then(function (response) {
+                            $scope.questionnaires = response;
+                        });
+                //TODO get checked surveys for this  patient_id       
+                        
+                $scope.hide = function () {
+                    $mdDialog.hide();
+                };
+
+                $scope.cancel = function () {
+                    $mdDialog.cancel();
+                };
+
+                $scope.answer = function (answer) {
+                    $mdDialog.hide(answer);
+                };
+
+                $scope.addQuestionnaire = function (questionnaire_id, selected) {
+                    if (selected){
+                        //TODO add
+                        console.log(questionnaire_id);
+                    } else {
+                        //TODO delete
+                        console.log("delete");
+                    }
+                    
+                }
+
+            }
+            ;
+
+        }
+
     }
 })();
 
